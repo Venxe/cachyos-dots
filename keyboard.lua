36bbe385cb07d9813f219423d6547597ee46f4a3
Venxe <ivenxe@gmail.com>
2026-06-27 20:44:13 +0300

    Update keyboard.lua

--- keyboard.lua
+++ keyboard.lua
@@ -1,1 +1,1 @@
-printf '\nhl.env("LANG", "tr_TR.UTF-8")\nhl.env("LC_ALL", "tr_TR.UTF-8")\n' >> ~/.config/caelestia/hypr-user.lua
+sed -i 's/kb_layout          = "us"/kb_layout          = "tr"/' ~/.config/hypr/hyprland/input.lua
