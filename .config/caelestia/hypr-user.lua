0bb1045931791cc0b72ea540484b47a7e61e1d13
Venxe <ivenxe@gmail.com>
2026-06-30 08:10:21 +0300

    Update hypr-user.lua

--- .config/caelestia/hypr-user.lua
+++ .config/caelestia/hypr-user.lua
@@ -1,6 +1,5 @@
 hl.on("hyprland.start", function()
     hl.exec_cmd("wl-clip-persist --clipboard regular")
-    hl.exec_cmd("udiskie -t")
 end)
 
 hl.monitor({ output = "DP-1", mode = "2560x1440@165.08Hz", position = "auto", scale = 1, })
