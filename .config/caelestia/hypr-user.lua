7bdffc4735a11470ebbb55d6f984b5c1641e181d
Venxe <ivenxe@gmail.com>
2026-07-03 20:18:02 +0300

    Update hypr-user.lua

--- .config/caelestia/hypr-user.lua
+++ .config/caelestia/hypr-user.lua
@@ -4,11 +4,10 @@ end)
 
 hl.monitor({ output = "DP-1", mode = "2560x1440@165.08Hz", position = "auto", scale = 1, })
 
+hl.env("XCURSOR_THEME", "Qogir-Dark")
+hl.env("XCURSOR_SIZE", "24")
+
 hl.config({
-    env = {
-        "XCURSOR_THEME,Qogir-Dark",
-        "XCURSOR_SIZE,24"
-    },
     input = {
         kb_layout = "tr",
     },
