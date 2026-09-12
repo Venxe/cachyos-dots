70d1adb70bb2dedc1e160f9f8fb86d090f69cbe5
Venxe <ivenxe@gmail.com>
2026-09-12 20:34:38 +0300

    feat: add tailscale and docker abbreviations

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,8 +1,14 @@
 abbr paclean 'sudo pacman -Rns (pacman -Qdtq)'
+
 abbr tsu 'sudo tailscale up'
 abbr tsd 'sudo tailscale down'
+
 abbr aquarium 'asciiquarium -t'
 abbr clock 'clock-rs'
 abbr matrix 'rmatrix'
 abbr pipes 'pipes-rs'
 abbr tetris 'vitetris'
+
+abbr lzd 'lazydocker'
+abbr dcup 'docker compose up -d'
+abbr dcdn 'docker compose down'
