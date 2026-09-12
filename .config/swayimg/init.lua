e1a1c243c5ccb2148f73bb92978cbc0027331071
Venxe <ivenxe@gmail.com>
2026-09-12 18:48:04 +0300

    feat: add mouse wheel navigation, image rotation, delete, and fs monitoring in swayimg config

--- .config/swayimg/init.lua
+++ .config/swayimg/init.lua
@@ -6,8 +6,9 @@ swayimg.exif_orientation = true
 swayimg.viewer.default_scale = "optimal"
 swayimg.viewer.default_position = "center"
 
--- Image List: Automatically add neighboring files in same folder for easy browsing
+-- Image List: Automatically add neighboring files in same folder and monitor FS changes
 swayimg.imagelist.adjacent = true
+swayimg.imagelist.fsmon = true
 
 -- Text Overlay (OSD): Hidden by default, 14px when toggled
 swayimg.text.visible = false
@@ -34,6 +35,41 @@ end)
 
 swayimg.viewer.on_key("space", function()
   swayimg.viewer.open("next")
+end)
+
+-- Mouse Wheel: ScrollDown -> next image, ScrollUp -> previous image
+swayimg.viewer.on_mouse("ScrollDown", function()
+  swayimg.viewer.open("next")
+end)
+
+swayimg.viewer.on_mouse("ScrollUp", function()
+  swayimg.viewer.open("prev")
+end)
+
+-- Rotate: 'r' for 90° clockwise, 'Shift+r' / 'R' for 90° counter-clockwise
+swayimg.viewer.on_key("r", function()
+  swayimg.viewer.rotate(90)
+end)
+
+swayimg.viewer.on_key({ "Shift+r", "Shift-r", "R" }, function()
+  swayimg.viewer.rotate(270)
+end)
+
+-- Trash / Delete: Send current image to Trash (gio trash) and advance to next image
+swayimg.viewer.on_key("Delete", function()
+  local img = swayimg.viewer.get_image()
+  if img and img.path then
+    os.execute(string.format("gio trash %q", img.path))
+    swayimg.imagelist.remove(img.path)
+  end
+end)
+
+swayimg.gallery.on_key("Delete", function()
+  local img = swayimg.gallery.get_image()
+  if img and img.path then
+    os.execute(string.format("gio trash %q", img.path))
+    swayimg.imagelist.remove(img.path)
+  end
 end)
 
 -- Toggle text overlay with 't'
