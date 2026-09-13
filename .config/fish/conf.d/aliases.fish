eaeb4c90656f942a1595afef7994e1b87459955e
Venxe <ivenxe@gmail.com>
2026-09-13 13:02:25 +0300

    chore: add recentclean abbreviation and reorder yazi

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,5 +1,8 @@
 abbr paclean 'sudo pacman -Rns (pacman -Qdtq)'
 
+abbr y 'yazi'
+abbr recentclean 'rm -f ~/.local/share/recently-used.xbel'
+
 abbr tss 'tailscale status'
 abbr tsu 'sudo tailscale up'
 abbr tsd 'sudo tailscale down'
@@ -19,5 +22,3 @@ abbr dcdn 'docker compose down'
 abbr dlopus 'yt-dlp --no-playlist -x --audio-format opus --embed-thumbnail --embed-metadata'
 abbr dlmkv 'yt-dlp --no-playlist -f "bestvideo+bestaudio/best" --merge-output-format mkv'
 abbr dlmp4 'yt-dlp --no-playlist -f "bestvideo+bestaudio/best" --merge-output-format mp4'
-
-abbr y 'yazi'
